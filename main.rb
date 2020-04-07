af2de27dae5cc5bf8c33758d54de4c3c071abafa
ToTkToSmOtRiT <zz90847@gmail.com>
2020-04-07 11:41:43 +0300

    deleted:    awre 	deleted:    glicerin 	deleted:    jubilant-goggles/glicerin 	modified:   main.rb 	modified:   my_complex.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,37 @@
-def File
-
+def main
+    g=0
+    arr=arr.to_a
+    b=b.to_a
+    puts 'Введите колличество элементов массива'
+    num=gets.to_i
+    #puts num
+    puts 'Введите элементы массива'
+    push(arr,num)
+    min(arr,num,g,b)
+    puts arr
+    puts b
+    puts g
 end
-def arr
-    a=a.to_a
-    a[1]=5
-    pp a[1]
+def min(arr,num,g,b)
+    i=0
+    h=0
+    while i<num-1 
+        if (arr[i]>0 && arr[i+1]<0) || (arr[i]<0 && arr[i+1]>0)
+            g=g+1
+            b[h]=i
+            b[h+1]=i+1
+            h=h+2
+        end
+    end
+end
+def push(arr,num)
+    i=0
+    while i<num
+        i=i+1
+        g=gets.to_i
+        arr[i]=g
+    end
+end
+if __FILE__ == $0
+    main
 end
